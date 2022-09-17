c2437eee97dff8bf7c55e21302d60579b9f59bac
YoungBin Choi <bin778@naver.com>
2022-09-17 15:50:55 +0900

    Update

--- ex/day03/src/day03/ArrList.java
+++ ex/day03/src/day03/ArrList.java
@@ -1,5 +1,30 @@
 package day03;
 
-public class ArrList {
+import java.util.NoSuchElementException;
 
+public class ArrList<E> {
+	private E a[];
+    private int size; // 리스트의 항목 수
+
+    // 배열 리스트
+    public ArrList() { // 생성자
+    	a = (E[])new Object[1]; // 1개의 원소를 가지는 배열
+    	size = 0; // 항목을 0으로 초기화
+    }
+    
+    // 연결(링크) 리스트
+    public class ListNode {
+        private int data; // 실제 노드
+        private ListNode next; // 리스트 노드
+        public ListNode(int data) {  }
+    }
+    
+    public E peek(int k) { // k번째 저장된 항목을 검색, a[k]
+    	if(size == 0) { // 항목(X)
+    		throw new NoSuchElementException(); // 프로그램 stop
+    	}
+    	return a[k];
+    }
+    
+    
 }
